358aa6f5d6abba2c31cf0294ff87774de07ea5b7
Anusha211209 <84378742+Anusha211209@users.noreply.github.com>
2022-06-06 16:00:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 420);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+    }
+}
+
+function draw(){
+    canvas.background('grey')
+}
